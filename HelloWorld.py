650260cb2f4666c555b3412d66468371cd68cd7c
Devin Duval <devinduval09@gmail.com>
2020-06-15 17:53:11 -0700

    git test

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,7 @@
 
 #Created on Jun 14, 2020
 
-@author: devin
+#@author: devin
 
 
 def helloworld(sentence):
@@ -12,7 +12,7 @@ def helloworld(sentence):
     popup.wm_title("")
     label2 = ttk.Label(popup,text = "Hello World", font=("Helvetica",20))
     label2.pack(side="top", ipadx=10, ipady=10)
-    label = ttk.Label(popup, text=sentence, font=("Helvetica",10))
+    label = ttk.Label(popup, text="git test", font=("Helvetica",10))
     label.pack(ipadx=10, ipady=10)
     B1 = ttk.Button(popup, text="Okay", command=quit)
     B1.pack(side="bottom", ipadx=10, ipady=10)
